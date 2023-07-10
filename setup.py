a65e540ffb26674394269874de1e924a78c5da42
chadjemmett <chad.jemmett@gmail.com>
2023-07-10 11:14:18 -0500

    Fixed spelling

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 setup(
-        name="handwriting-synthesit",
+        name="handwriting_synthesis",
         version="0.0.1",
         packages=find_packages(),
         install_requires=[
